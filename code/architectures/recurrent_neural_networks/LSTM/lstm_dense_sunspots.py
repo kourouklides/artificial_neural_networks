c7e64247b49e6215da815384e9c60ef75eeb7f34
kourouklides <leonariso@gmail.com>
2018-11-18 13:12:44 +0200

    Update code/architectures/recurrent_neural_networks/LSTM/lstm_dense_sunspots.py

--- code/architectures/recurrent_neural_networks/LSTM/lstm_dense_sunspots.py
+++ code/architectures/recurrent_neural_networks/LSTM/lstm_dense_sunspots.py
@@ -8,62 +8,54 @@ Dataset: Monthly sunspots
 Task: Time Series Forecasting (Univariate Regression)
 
     Author: Ioannis Kourouklides, www.kourouklides.com
-    License: https://github.com/kourouklides/artificial_neural_networks/blob/master/LICENSE
+    License:
+        https://github.com/kourouklides/artificial_neural_networks/blob/master/LICENSE
 
 """
 # %%
-# Python configurations
+# IMPORTS
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as np
-import tensorflow as tf
+# standard library imports
+import argparse
+from math import sqrt
+import os
 import random as rn
+from timeit import default_timer as timer
 
+# third-party imports
+from keras import backend as K
 from keras import optimizers
+from keras.callbacks import ModelCheckpoint
 from keras.layers import Input, Dense, LSTM
 from keras.models import Model
-from keras.callbacks import ModelCheckpoint
-from keras import backend as K
-
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from math import sqrt
-
-import json
-import yaml
-
-import argparse
-
-import os
-
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import tensorflow as tf
 
 # %%
 
 
-def none_or_int(value):
-    if value == 'None':
-        return None
-    else:
-        return int(value)
-
-
-def none_or_float(value):
-    if value == 'None':
-        return None
-    else:
-        return float(value)
-
-
 def lstm_dense_sunsposts(new_dir=os.getcwd()):
+    """
+    Main function
+    """
+    # %%
+    # IMPORTS
 
     os.chdir(new_dir)
 
-    from artificial_neural_networks.code.utils.download_monthly_sunspots \
-        import download_monthly_sunspots
+    # code repository sub-package imports
+    from artificial_neural_networks.code.utils.download_monthly_sunspots import \
+        download_monthly_sunspots
+    from artificial_neural_networks.code.utils.generic_utils import none_or_int, none_or_float, \
+        save_regress_model, series_to_supervised, affine_transformation
 
+    # %%
     # SETTINGS
     parser = argparse.ArgumentParser()
 
@@ -71,11 +63,12 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
     parser.add_argument('--verbose', type=int, default=1)
     parser.add_argument('--reproducible', type=bool, default=True)
     parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--time_training', type=bool, default=True)
     parser.add_argument('--plot', type=bool, default=False)
 
     # Settings for preprocessing and hyperparameters
     parser.add_argument('--look_back', type=int, default=3)
-    parser.add_argument('--scaling_factor', type=float, default=(1/780))
+    parser.add_argument('--scaling_factor', type=float, default=(1 / 780))
     parser.add_argument('--translation', type=float, default=0)
     parser.add_argument('--layer_size', type=int, default=4)
     parser.add_argument('--n_epochs', type=int, default=13)
@@ -94,41 +87,32 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
 
     args = parser.parse_args()
 
-    if (args.verbose > 0):
+    if args.verbose > 0:
         print(args)
 
     # For reproducibility
-    if (args.reproducible):
+    if args.reproducible:
         os.environ['PYTHONHASHSEED'] = '0'
         np.random.seed(args.seed)
         rn.seed(args.seed)
         tf.set_random_seed(args.seed)
+        sess = tf.Session(graph=tf.get_default_graph())
+        K.set_session(sess)
+        # print(hash("keras"))
 
     # %%
     # Load the Monthly sunspots dataset
 
     sunspots_path = download_monthly_sunspots()
-    sunspots = np.genfromtxt(fname=sunspots_path, dtype=np.float32,
-                             delimiter=",", skip_header=1, usecols=1)
+    sunspots = np.genfromtxt(
+        fname=sunspots_path, dtype=np.float32, delimiter=",", skip_header=1, usecols=1)
 
     # %%
     # Train-Test split
 
-    def series_to_supervised(dataset, look_back=1):
-        # Prepare the dataset (Time Series) to be used for Supervised Learning
-
-        data_X, data_Y = [], []
-
-        for i in range(len(dataset) - look_back):
-            sliding_window = i + look_back
-            data_X.append(dataset[i:sliding_window])
-            data_Y.append(dataset[sliding_window])
-
-        return np.array(data_X), np.array(data_Y)
-
     n_series = len(sunspots)
 
-    split_ratio = 2/3  # between zero and one
+    split_ratio = 2 / 3  # between zero and one
     n_split = int(n_series * split_ratio)
 
     look_back = args.look_back
@@ -155,18 +139,6 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
     train_x = train_x.reshape(n_train, n_in, 1)
     test_x = test_x.reshape(n_test, n_in, 1)
 
-    def affine_transformation(data_in, scaling, translation, inverse=False):
-        # Apply affine trasnforamtion to the data
-
-        if (inverse):
-            # Inverse Transformation
-            data_out = (data_in / scaling) + translation
-        else:
-            # Direct Transformation
-            data_out = scaling * (data_in - translation)
-
-        return data_out
-
     # Apply preprocessing
     train_x_ = affine_transformation(train_x, scaling_factor, translation)
     train_y_ = affine_transformation(train_y, scaling_factor, translation)
@@ -187,11 +159,11 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
 
     model = Model(inputs=x, outputs=out)
 
-    if (args.verbose > 0):
+    if args.verbose > 0:
         model.summary()
 
     def root_mean_squared_error(y_true, y_pred):
-            return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
+        return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
 
     loss_function = root_mean_squared_error
 
@@ -199,29 +171,26 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
 
     lr = args.lrearning_rate
     epsilon = args.epsilon
-    optimizer_selection = {'Adadelta':  optimizers.Adadelta(
-                                   lr=lr, rho=0.95, epsilon=epsilon, decay=0.0),
-                           'Adagrad':   optimizers.Adagrad(
-                                   lr=lr, epsilon=epsilon, decay=0.0),
-                           'Adam':      optimizers.Adam(
-                                   lr=lr, beta_1=0.9, beta_2=0.999,
-                                   epsilon=epsilon, decay=0.0, amsgrad=False),
-                           'Adamax':    optimizers.Adamax(
-                                   lr=lr, beta_1=0.9, beta_2=0.999,
-                                   epsilon=epsilon, decay=0.0),
-                           'Nadam':     optimizers.Nadam(
-                                   lr=lr, beta_1=0.9, beta_2=0.999,
-                                   epsilon=epsilon, schedule_decay=0.004),
-                           'RMSprop':   optimizers.RMSprop(
-                                   lr=lr, rho=0.9, epsilon=epsilon, decay=0.0),
-                           'SGD':       optimizers.SGD(
-                                   lr=lr, momentum=0.0, decay=0.0, nesterov=False)}
+    optimizer_selection = {
+        'Adadelta':
+        optimizers.Adadelta(lr=lr, rho=0.95, epsilon=epsilon, decay=0.0),
+        'Adagrad':
+        optimizers.Adagrad(lr=lr, epsilon=epsilon, decay=0.0),
+        'Adam':
+        optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=epsilon, decay=0.0, amsgrad=False),
+        'Adamax':
+        optimizers.Adamax(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=epsilon, decay=0.0),
+        'Nadam':
+        optimizers.Nadam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=epsilon, schedule_decay=0.004),
+        'RMSprop':
+        optimizers.RMSprop(lr=lr, rho=0.9, epsilon=epsilon, decay=0.0),
+        'SGD':
+        optimizers.SGD(lr=lr, momentum=0.0, decay=0.0, nesterov=False)
+    }
 
     optimizer = optimizer_selection[args.optimizer]
 
-    model.compile(optimizer=optimizer,
-                  loss=loss_function,
-                  metrics=metrics)
+    model.compile(optimizer=optimizer, loss=loss_function, metrics=metrics)
 
     # %%
     # Save trained models for every epoch
@@ -232,7 +201,7 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
     model_path = models_path + model_name + '_model'
     file_suffix = '_{epoch:04d}_{val_loss:.4f}_{val_mean_absolute_error:.4f}'
 
-    if (args.save_weights_only):
+    if args.save_weights_only:
         file_path = weights_path
     else:
         file_path = model_path
@@ -241,13 +210,14 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
 
     monitor = 'val_loss'
 
-    if (args.save_models):
-        checkpoint = ModelCheckpoint(file_path + '.h5',
-                                     monitor=monitor,
-                                     verbose=args.verbose,
-                                     save_best_only=args.save_best,
-                                     mode='auto',
-                                     save_weights_only=args.save_weights_only)
+    if args.save_models:
+        checkpoint = ModelCheckpoint(
+            file_path + '.h5',
+            monitor=monitor,
+            verbose=args.verbose,
+            save_best_only=args.save_best,
+            mode='auto',
+            save_weights_only=args.save_weights_only)
         callbacks = [checkpoint]
     else:
         callbacks = []
@@ -255,12 +225,23 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
     # %%
     # TRAINING PHASE
 
-    model_history = model.fit(x=train_x_, y=train_y_,
-                              validation_data=(test_x_, test_y_),
-                              batch_size=args.batch_size,
-                              epochs=args.n_epochs,
-                              verbose=args.verbose,
-                              callbacks=callbacks)
+    if args.time_training:
+        start = timer()
+
+    model.fit(
+        x=train_x_,
+        y=train_y_,
+        validation_data=(test_x_, test_y_),
+        batch_size=args.batch_size,
+        epochs=args.n_epochs,
+        verbose=args.verbose,
+        callbacks=callbacks)
+
+    if args.time_training:
+        end = timer()
+        duration = end - start
+        print('Total time for training (in seconds):')
+        print(duration)
 
     # %%
     # TESTING PHASE
@@ -270,10 +251,8 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
     test_y_pred_ = model.predict(test_x_)[:, 0]
 
     # Remove preprocessing
-    train_y_pred = affine_transformation(train_y_pred_, scaling_factor, translation,
-                                         inverse=True)
-    test_y_pred = affine_transformation(test_y_pred_, scaling_factor, translation,
-                                        inverse=True)
+    train_y_pred = affine_transformation(train_y_pred_, scaling_factor, translation, inverse=True)
+    test_y_pred = affine_transformation(test_y_pred_, scaling_factor, translation, inverse=True)
 
     train_rmse = sqrt(mean_squared_error(train_y, train_y_pred))
     train_mae = mean_absolute_error(train_y, train_y_pred)
@@ -283,7 +262,7 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
     test_mae = mean_absolute_error(test_y, test_y_pred)
     test_r2 = r2_score(test_y, test_y_pred)
 
-    if (args.verbose > 0):
+    if args.verbose > 0:
         print('Train RMSE: %.4f ' % (train_rmse))
         print('Train MAE: %.4f ' % (train_mae))
         print('Train (1 - R_squared): %.4f ' % (1.0 - train_r2))
@@ -297,7 +276,7 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
     # %%
     # Data Visualization
 
-    if (args.plot):
+    if args.plot:
         plt.figure()
         plt.plot(train_y)
         plt.plot(train_y_pred)
@@ -310,14 +289,14 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
         plt.title('Time Series of the test set')
         plt.show()
 
-        plt.figure()
         train_errors = train_y - train_y_pred
+        plt.figure()
         plt.hist(train_errors, bins='auto')
         plt.title('Histogram of training errors')
         plt.show()
 
-        plt.figure()
         test_errors = test_y - test_y_pred
+        plt.figure()
         plt.hist(test_errors, bins='auto')
         plt.title('Histogram of test errors')
         plt.show()
@@ -349,33 +328,15 @@ def lstm_dense_sunsposts(new_dir=os.getcwd()):
     # %%
     # Save the architecture and the lastly trained model
 
-    architecture_path = models_path + model_name + '_architecture'
+    save_regress_model(model, models_path, model_name, weights_path, model_path, file_suffix,
+                       test_rmse, test_mae, args)
 
-    last_suffix = file_suffix.format(epoch=args.n_epochs,
-                                     val_loss=test_rmse,
-                                     val_mean_absolute_error=test_mae)
-
-    if (args.save_architecture):
-        # Save only the archtitecture (as a JSON file)
-        json_string = model.to_json()
-        json.dump(json.loads(json_string), open(architecture_path + '.json', "w"))
-
-        # Save only the archtitecture (as a YAML file)
-        yaml_string = model.to_yaml()
-        yaml.dump(yaml.load(yaml_string), open(architecture_path + '.yml', "w"))
-
-    # Save only the weights (as an HDF5 file)
-    if (args.save_last_weights):
-        model.save_weights(weights_path + last_suffix + '.h5')
-
-    # Save the whole model (as an HDF5 file)
-    if (args.save_last_model):
-        model.save(model_path + last_suffix + '.h5')
+    # %%
 
     return model
 
+
 # %%
 
-
 if __name__ == '__main__':
-    model = lstm_dense_sunsposts('../../../../../')
+    model_lstm_dense = lstm_dense_sunsposts('../../../../../')
