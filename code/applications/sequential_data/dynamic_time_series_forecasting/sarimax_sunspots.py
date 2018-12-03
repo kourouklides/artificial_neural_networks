b31f62237f4fbc4129fdd0cb875452230d81dd7a
kourouklides <leonariso@gmail.com>
2018-12-03 02:32:27 +0200

    Update code/applications/sequential_data/dynamic_time_series_forecasting/sarimax_sunspots.py

--- code/applications/sequential_data/dynamic_time_series_forecasting/sarimax_sunspots.py
+++ code/applications/sequential_data/dynamic_time_series_forecasting/sarimax_sunspots.py
@@ -34,7 +34,7 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 # %%
 
 
-def main(new_dir=os.getcwd()):
+def sarimax_sunspots(new_dir=os.getcwd()):
     """
     Main function
     """
@@ -62,9 +62,10 @@ def main(new_dir=os.getcwd()):
     # Settings for preprocessing and hyperparameters
     parser.add_argument('--scaling_factor', type=float, default=(1 / 1))
     parser.add_argument('--translation', type=float, default=0)
-    parser.add_argument('--autoregressive', type=float, default=1)
-    parser.add_argument('--integrated', type=float, default=0)
-    parser.add_argument('--moving_average', type=float, default=1)
+    parser.add_argument('--autoregressive', type=int, default=1)
+    parser.add_argument('--integrated', type=int, default=0)
+    parser.add_argument('--moving_average', type=int, default=1)
+    parser.add_argument('--seasonal_periods', type=int, default=128)
 
     args = parser.parse_args()
 
@@ -106,6 +107,7 @@ def main(new_dir=os.getcwd()):
 
     # Apply preprocessing
     train_y_ = affine_transformation(train_y, scaling_factor, translation)
+    test_y_ = affine_transformation(test_y, scaling_factor, translation)
 
     # %%
     # Model hyperparameters
@@ -116,8 +118,10 @@ def main(new_dir=os.getcwd()):
     maxiter = 1
     # maxiter = 50
 
+    s = args.seasonal_periods
     order = (args.autoregressive, args.integrated, args.moving_average)
-    seasonal_order = (0, 1, 0, 130)
+    seasonal_order = (0, 1, 0, s)
+    trend = 'ct'
 
     # %%
     # TRAINING PHASE
@@ -125,7 +129,7 @@ def main(new_dir=os.getcwd()):
     train_outliers = np.zeros(n_train)
 
     train_model = SARIMAX(train_y_, order=order, seasonal_order=seasonal_order,
-                          exog=train_outliers, trend='t')
+                          exog=train_outliers, trend=trend)
 
     if args.time_training:
         start = timer()
@@ -134,12 +138,17 @@ def main(new_dir=os.getcwd()):
 
     for i in range(1):
         model_fit = train_model.fit(start_params=fitted_params, method=optimizer, maxiter=maxiter)
-        model_fit.params = [4.76187914e-01, 5.03594751e-04, 0.00000000e+00, 9.40341508e-01, -3.77361530e-01, 4.68511922e+02]
         fitted_params = model_fit.params
 
+        new_params = fitted_params.copy()
+        new_params[0] = 0.8446147426983434  # 0.4446147426983434
+        new_params[1] = -0.00087190913463951184  # -0.00047190913463951184
+
+        model_fit.params = new_params
+
         if args.verbose > 0:
-            print('Fitted parameters:')
-            print(fitted_params)
+            print('All fitted parameters:')
+            print(model_fit.params)
 
     if args.time_training:
         end = timer()
@@ -153,11 +162,22 @@ def main(new_dir=os.getcwd()):
     # %%
     # TESTING PHASE
 
-    test_outliers = np.zeros((n_test,1))
+    test_outliers = np.zeros((n_test, 1))
+
+    test_model = SARIMAX(test_y_, order=order, seasonal_order=seasonal_order,
+                         exog=test_outliers, trend=trend)
 
     # Predict preprocessed values
-    train_y_pred_ = model_fit.predict(start=0, end=n_train-1, exog=test_outliers)
-    test_y_pred_ = model_fit.predict(start=n_train, end=n_series-1, exog=test_outliers)
+    train_y_pred_ = np.zeros(n_train)
+    train_y_pred_[s:] = train_model.filter(new_params).get_prediction(
+            start=s, end=n_train-1, exog=train_outliers, dynamic=True).predicted_mean
+    # train_y_pred_[s:] = model_fit.predict(start=s, end=n_train-1, exog=test_outliers,dynamic=True)
+    test_y_pred_ = np.zeros(n_test)
+    test_y_pred_[s:] = test_model.filter(new_params).get_prediction(
+            start=s, end=n_test-1, exog=test_outliers, dynamic=True).predicted_mean
+    # test_y_pred_[s:] = model_fit.predict(start=n_train+s, end=n_series-1, exog=test_outliers)
+
+    # TODO: change both to filters
 
     # Remove preprocessing
     train_y_pred = affine_transformation(train_y_pred_, scaling_factor, translation, inverse=True)
@@ -234,9 +254,20 @@ def main(new_dir=os.getcwd()):
         plt.title('Residuals vs Predicted for test set')
         plt.show()
 
+    # %%
+
+    model = {}
+    model['params'] = new_params
+    model['hyperparams'] = {}
+    model['hyperparams']['order'] = order
+    model['hyperparams']['seasonal_order'] = seasonal_order
+    model['hyperparams']['trend'] = trend
+
+    return model
+
 
 # %%
 
 
 if __name__ == '__main__':
-    main('../../../../../')
+    model_sarimax_sunspots = sarimax_sunspots('../../../../../')
