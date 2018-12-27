ca3fc75301e99a9cb54e68bad2323da35b2aa041
kourouklides <leonariso@gmail.com>
2018-12-27 03:36:15 +0200

    Update bi_lstm_dropout_sunspots.py

--- code/applications/sequential_data/one_step_time_series_forecasting/bi_lstm_dropout_sunspots.py
+++ code/applications/sequential_data/one_step_time_series_forecasting/bi_lstm_dropout_sunspots.py
@@ -20,16 +20,60 @@ from __future__ import division
 from __future__ import print_function
 
 # standard library imports
+import argparse
 import os
 
 # %%
 
 
 if __name__ == '__main__':
+
+    # %%
+    # IMPORTS
+
     os.chdir('../../../../../')
 
     # code repository sub-package imports
     from artificial_neural_networks.code.architectures.recurrent_neural_networks.LSTM. \
         bi_lstm_dropout_sunspots import bi_lstm_dropout_sunspots
+    from artificial_neural_networks.code.utils.generic_utils import none_or_int, none_or_float
+
+    # %%
+    # SETTINGS
+
+    parser = argparse.ArgumentParser()
+
+    # General settings
+    parser.add_argument('--verbose', type=int, default=1)
+    parser.add_argument('--reproducible', type=bool, default=True)
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--time_training', type=bool, default=True)
+    parser.add_argument('--plot', type=bool, default=False)
+
+    # Settings for preprocessing and hyperparameters
+    parser.add_argument('--look_back', type=int, default=3)
+    parser.add_argument('--scaling_factor', type=float, default=(1 / 780))
+    parser.add_argument('--translation', type=float, default=0)
+    parser.add_argument('--layer_size', type=int, default=4)
+    parser.add_argument('--n_epochs', type=int, default=13)
+    parser.add_argument('--batch_size', type=none_or_int, default=1)
+    parser.add_argument('--optimizer', type=str, default='Adam')
+    parser.add_argument('--lrearning_rate', type=float, default=1e-3)
+    parser.add_argument('--epsilon', type=none_or_float, default=None)
+    parser.add_argument('--dropout_rate_input', type=int, default=0.01)
+    parser.add_argument('--dropout_rate_hidden', type=int, default=0.01)
+
+    # Settings for saving the model
+    parser.add_argument('--save_architecture', type=bool, default=True)
+    parser.add_argument('--save_last_weights', type=bool, default=True)
+    parser.add_argument('--save_last_model', type=bool, default=True)
+    parser.add_argument('--save_models', type=bool, default=False)
+    parser.add_argument('--save_weights_only', type=bool, default=False)
+    parser.add_argument('--save_best', type=bool, default=True)
+
+    args = parser.parse_args()
+
+    # %%
+    # MODEL
 
     model_bi_lstm_dense = bi_lstm_dropout_sunspots()
