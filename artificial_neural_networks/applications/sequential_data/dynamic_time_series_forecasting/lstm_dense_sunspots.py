dc206b5bc0ec3c5196daaae7a7dd15d28413f7ee
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2019-10-29 23:24:13 +0200

    Update lstm_dense_sunspots.py

--- artificial_neural_networks/applications/sequential_data/dynamic_time_series_forecasting/lstm_dense_sunspots.py
+++ artificial_neural_networks/applications/sequential_data/dynamic_time_series_forecasting/lstm_dense_sunspots.py
@@ -48,11 +48,11 @@ def lstm_dense_sunspots(args):
     # IMPORTS
 
     # code repository sub-package imports
-    from artificial_neural_networks.code.utils.download_monthly_sunspots import \
+    from artificial_neural_networks.utils.download_monthly_sunspots import \
         download_monthly_sunspots
-    from artificial_neural_networks.code.utils.generic_utils import save_regress_model,  \
+    from artificial_neural_networks.utils.generic_utils import save_regress_model,  \
         series_to_supervised, affine_transformation
-    from artificial_neural_networks.code.utils.vis_utils import regression_figs
+    from artificial_neural_networks.utils.vis_utils import regression_figs
 
     # %%
 
@@ -420,7 +420,7 @@ if __name__ == '__main__':
     os.chdir('../../../../../')
 
     # code repository sub-package imports
-    from artificial_neural_networks.code.utils.generic_utils import none_or_int, none_or_float
+    from artificial_neural_networks.utils.generic_utils import none_or_int, none_or_float
 
     # %%
     # SETTINGS
