cfb1ecc63ec57388c1d6d2872be737b517ddefdf
kourouklides <leonariso@gmail.com>
2019-10-30 18:05:35 +0200

    Create transformer_anki.py

---
+++ artificial_neural_networks/applications/natural_language_processing/machine_translation/transformer_anki.py
@@ -0,0 +1,14 @@
+"""
+
+Model: Transformer (i.e. FNN with self-attention)
+Mehtod: Backpropagation
+Architecture: Recurrent Neural Network
+
+Dataset: Anki
+Task: Machine Translation
+
+    Author: Ioannis Kourouklides, www.kourouklides.com
+    License:
+        https://github.com/kourouklides/artificial_neural_networks/blob/master/LICENSE
+
+"""
