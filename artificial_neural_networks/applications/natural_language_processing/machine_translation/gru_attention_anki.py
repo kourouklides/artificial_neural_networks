690b63b9b5ef3ea325aced6400eeb9ae9ce1ee8e
kourouklides <leonariso@gmail.com>
2019-10-30 18:08:03 +0200

    Create gru_attention_anki.py

---
+++ artificial_neural_networks/applications/natural_language_processing/machine_translation/gru_attention_anki.py
@@ -0,0 +1,14 @@
+"""
+
+Model: GRU with attention
+Mehtod: Truncated Backpropagation Through Time (TBPTT)
+Architecture: Recurrent Neural Network
+
+Dataset: Anki
+Task: Machine Translation
+
+    Author: Ioannis Kourouklides, www.kourouklides.com
+    License:
+        https://github.com/kourouklides/artificial_neural_networks/blob/master/LICENSE
+
+"""
