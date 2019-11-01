dc5e8d05fd9bdcedc71288f7d48c57f0bdaba851
kourouklides <leonariso@gmail.com>
2019-11-01 22:36:08 +0200

    Update transformer_ted.py

--- artificial_neural_networks/applications/natural_language_processing/machine_translation/transformer_ted.py
+++ artificial_neural_networks/applications/natural_language_processing/machine_translation/transformer_ted.py
@@ -33,7 +33,7 @@ import tensorflow_datasets as tfds
 
 
 # Enable eager execution in TensorFlow
-tf.compat.v1.enable_eager_execution()
+tf.enable_eager_execution()
 
 
 # %%
@@ -359,6 +359,47 @@ class Decoder(tf.keras.layers.Layer):
         return x, attention_weights
 
 
+class Transformer(tf.keras.Model):
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size,
+                 target_vocab_size, pe_input, pe_target, rate=0.1):
+        super(Transformer, self).__init__()
+
+        self.encoder = Encoder(num_layers, d_model, num_heads, dff,
+                               input_vocab_size, pe_input, rate)
+
+        self.decoder = Decoder(num_layers, d_model, num_heads, dff,
+                               target_vocab_size, pe_target, rate)
+
+        self.final_layer = tf.keras.layers.Dense(target_vocab_size)
+
+    def __call__(self, inp, tar, training, enc_padding_mask, look_ahead_mask, dec_padding_mask):
+
+        enc_output = self.encoder(inp, training,
+                                  enc_padding_mask)  # (batch_size, inp_seq_len, d_model)
+
+        # dec_output.shape == (batch_size, tar_seq_len, d_model)
+        dec_output, attention_weights = self.decoder(
+            tar, enc_output, training, look_ahead_mask, dec_padding_mask)
+
+        final_output = self.final_layer(dec_output)  # (batch_size, tar_seq_len, target_vocab_size)
+
+        return final_output, attention_weights
+
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    def __init__(self, d_model, warmup_steps=4000):
+        super(CustomSchedule, self).__init__()
+
+        self.d_model = d_model
+        self.d_model = tf.cast(self.d_model, tf.float32)
+
+        self.warmup_steps = warmup_steps
+
+    def __call__(self, step):
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
+
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
 
 
 # %%
@@ -402,6 +443,25 @@ val_dataset = val_dataset.filter(filter_max_length).padded_batch(
 input_vocab_size = tokenizer_pt.vocab_size + 2
 target_vocab_size = tokenizer_en.vocab_size + 2
 
+learning_rate = CustomSchedule(d_model)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_object(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+
+    return tf.reduce_mean(loss_)
+
+
+# %%
+# Save trained models for every epoch
 
 
 # %%
@@ -525,10 +585,27 @@ output, attn = sample_decoder(temp_input,
 output.shape, attn['decoder_layer2_block2'].shape
 
 
+sample_transformer = Transformer(
+    num_layers=2, d_model=512, num_heads=8, dff=2048,
+    input_vocab_size=8500, target_vocab_size=8000,
+    pe_input=10000, pe_target=6000)
+
+temp_input = tf.random.uniform((64, 38), dtype=tf.int64, minval=0, maxval=200)
+temp_target = tf.random.uniform((64, 36), dtype=tf.int64, minval=0, maxval=200)
+
+fn_out, _ = sample_transformer(temp_input, temp_target, training=False,
+                               enc_padding_mask=None,
+                               look_ahead_mask=None,
+                               dec_padding_mask=None)
+
+fn_out.shape  # (batch_size, tar_seq_len, target_vocab_size)
 
 
+temp_learning_rate_schedule = CustomSchedule(d_model)
 
-
+plt.plot(temp_learning_rate_schedule(tf.range(4e4, dtype=np.float32)).numpy())
+plt.ylabel("Learning Rate")
+plt.xlabel("Train Step")
 
 
 
