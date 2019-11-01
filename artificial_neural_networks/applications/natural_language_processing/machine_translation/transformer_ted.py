710cbede9eb35238be69f29351d7c394e473cd69
kourouklides <leonariso@gmail.com>
2019-11-01 23:12:16 +0200

    Update transformer_ted.py

--- artificial_neural_networks/applications/natural_language_processing/machine_translation/transformer_ted.py
+++ artificial_neural_networks/applications/natural_language_processing/machine_translation/transformer_ted.py
@@ -603,6 +603,57 @@ def evaluate(inp_sentence):
     return tf.squeeze(output, axis=0), attention_weights
 
 
+def plot_attention_weights(attention, sentence, result, layer):
+    fig = plt.figure(figsize=(16, 8))
+
+    sentence = tokenizer_pt.encode(sentence)
+
+    attention = tf.squeeze(attention[layer], axis=0)
+
+    for head in range(attention.shape[0]):
+        ax = fig.add_subplot(2, 4, head+1)
+
+        # plot the attention weights
+        ax.matshow(attention[head][:-1, :], cmap='viridis')
+
+        fontdict = {'fontsize': 10}
+
+        ax.set_xticks(range(len(sentence)+2))
+        ax.set_yticks(range(len(result)))
+
+        ax.set_ylim(len(result)-1.5, -0.5)
+
+        ax.set_xticklabels(
+            ['<start>']+[tokenizer_pt.decode([i]) for i in sentence]+['<end>'],
+            fontdict=fontdict, rotation=90)
+
+        ax.set_yticklabels([tokenizer_en.decode([i]) for i in result
+                            if i < tokenizer_en.vocab_size],
+                           fontdict=fontdict)
+
+        ax.set_xlabel('Head {}'.format(head+1))
+
+    plt.tight_layout()
+    plt.show()
+
+
+def translate(sentence, plot=''):
+    result, attention_weights = evaluate(sentence)
+
+    predicted_sentence = tokenizer_en.decode([i for i in result if i < tokenizer_en.vocab_size])
+
+    print('Input: {}'.format(sentence))
+    print('Predicted translation: {}'.format(predicted_sentence))
+
+    if plot:
+        plot_attention_weights(attention_weights, sentence, result, plot)
+
+
+translate("este é um problema que temos que resolver.")
+print("Real translation: this is a problem we have to solve .")
+
+translate("este é o primeiro livro que eu fiz.", plot='decoder_layer4_block2')
+print("Real translation: this is the first book i've ever done.")
 
 
 # %%
