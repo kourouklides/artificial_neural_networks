951c662a7fefaac81ccc37361220138144024123
kourouklides <leonariso@gmail.com>
2019-11-01 04:54:38 +0200

    Update gru_shakespeare.py

--- artificial_neural_networks/applications/natural_language_processing/text_generation/gru_shakespeare.py
+++ artificial_neural_networks/applications/natural_language_processing/text_generation/gru_shakespeare.py
@@ -22,6 +22,7 @@ from __future__ import unicode_literals
 
 
 # standard library imports
+import argparse
 import os
 import time
 
@@ -36,204 +37,203 @@ tf.compat.v1.enable_eager_execution()
 
 
 # %%
+# SETTINGS
 
-path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+parser = argparse.ArgumentParser()
 
-# Read, then decode for py2 compat.
-text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
-# length of text is the number of characters in it
-print ('Length of text: {} characters'.format(len(text)))
+# General settings
+parser.add_argument('--verbose', type=int, default=1)
 
-# The unique characters in the file
-vocab = sorted(set(text))
-print ('{} unique characters'.format(len(vocab)))
+args = parser.parse_args()
 
-# Creating a mapping from unique characters to indices
-char2idx = {u:i for i, u in enumerate(vocab)}
-idx2char = np.array(vocab)
 
-text_as_int = np.array([char2idx[c] for c in text])
-
-# The maximum length sentence we want for a single input in characters
 seq_length = 100
-examples_per_epoch = len(text)//seq_length
+BATCH_SIZE = 256
+embedding_dim = 256
+rnn_units = 256
+EPOCHS = 10
 
-# Create training examples / targets
-char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
 
-for i in char_dataset.take(5):
-  print(idx2char[i.numpy()])
-
-sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
-
-for item in sequences.take(5):
-  print(repr(''.join(idx2char[item.numpy()])))
+# %%
+# Utility functions
 
 def split_input_target(chunk):
     input_text = chunk[:-1]
     target_text = chunk[1:]
+
     return input_text, target_text
+
+
+# %%
+# Model
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size, rnn):
+    model = tf.keras.Sequential([
+            tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                                      batch_input_shape=[batch_size, None]),
+            rnn(rnn_units,
+                return_sequences=True,
+                recurrent_initializer='glorot_uniform',
+                stateful=True),
+            tf.keras.layers.Dense(vocab_size)])
+
+    return model
+
+
+# %%
+# Load the Shakespeare dataset
+
+path_to_file = tf.keras.utils.get_file(
+        'shakespeare.txt',
+        'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
+# Read, then decode for py2 compat.
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+
+if args.verbose > 0:
+    # length of text is the number of characters in it
+    print('Length of text: {} characters'.format(len(text)))
+
+    # The unique characters in the file
+    vocab = sorted(set(text))
+    print('{} unique characters'.format(len(vocab)))
+
+
+# %%
+# Training set
+
+BUFFER_SIZE = 10000
+examples_per_epoch = len(text)//seq_length
+steps_per_epoch = examples_per_epoch // BATCH_SIZE
+vocab_size = len(vocab)
+
+# Creating a mapping from unique characters to indices
+char2idx = {u: i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[c] for c in text])
+
+# Create training examples and targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
 
 dataset = sequences.map(split_input_target)
 
-
-for input_example, target_example in dataset.take(1):
-  print ('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
-  print ('Target data:', repr(''.join(idx2char[target_example.numpy()])))
-
-
-for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
-    print("Step {:4d}".format(i))
-    print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
-    print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
-
-
-# Batch size
-BATCH_SIZE = 256
-steps_per_epoch = examples_per_epoch//BATCH_SIZE
-
-# Buffer size to shuffle the dataset
-# (TF data is designed to work with possibly infinite sequences,
-# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
-# it maintains a buffer in which it shuffles elements).
-BUFFER_SIZE = 10000
-
 dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 
-# Length of the vocabulary in chars
-vocab_size = len(vocab)
-
-# The embedding dimension
-embedding_dim = 256
-
-# Number of RNN units
-rnn_units = 256
-
-
-if tf.test.is_gpu_available():
-  rnn = tf.keras.layers.CuDNNGRU
-else:
-  import functools
-  rnn = functools.partial(
-    tf.keras.layers.GRU, recurrent_activation='sigmoid')
-
-
-def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
-  model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size, embedding_dim,
-                              batch_input_shape=[batch_size, None]),
-    rnn(rnn_units,
-        return_sequences=True,
-        recurrent_initializer='glorot_uniform',
-        stateful=True),
-    tf.keras.layers.Dense(vocab_size)
-  ])
-  return model
 
 # %%
+# Model hyperparameters and ANN Architecture
+
+if tf.test.is_gpu_available():
+    RNN = tf.keras.layers.CuDNNGRU
+else:
+    import functools
+    RNN = functools.partial(tf.keras.layers.GRU, recurrent_activation='sigmoid')
 
 model = build_model(
-  vocab_size = len(vocab),
-  embedding_dim=embedding_dim,
-  rnn_units=rnn_units,
-  batch_size=BATCH_SIZE)
-
-for input_example_batch, target_example_batch in dataset.take(1):
-  example_batch_predictions = model(input_example_batch)
-  print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+        vocab_size=len(vocab),
+        embedding_dim=embedding_dim,
+        rnn_units=rnn_units,
+        batch_size=BATCH_SIZE,
+        rnn=RNN)
 
 model.summary()
 
-sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
-sampled_indices = tf.squeeze(sampled_indices,axis=-1).numpy()
 
 def loss(labels, logits):
-  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+    return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
-example_batch_loss  = loss(target_example_batch, example_batch_predictions)
-print("Prediction shape: ", example_batch_predictions.shape, " # (batch_size, sequence_length, vocab_size)")
-print("scalar_loss:      ", example_batch_loss.numpy().mean())
 
-model.compile(
-    optimizer = tf.compat.v1.train.AdamOptimizer(),
-    loss = loss)
+model.compile(optimizer=tf.compat.v1.train.AdamOptimizer(), loss=loss)
+
+
+# %%
+# Save trained models for every epoch
 
 # Directory where the checkpoints will be saved
 checkpoint_dir = './training_checkpoints'
 # Name of the checkpoint files
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
 
-checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_prefix,
-    save_weights_only=True)
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_prefix,
+        save_weights_only=True)
 
-EPOCHS = 10
+
+# %%
+# TRAINING PHASE
 
 history = model.fit(dataset.repeat(), epochs=EPOCHS, steps_per_epoch=steps_per_epoch,
-                     callbacks=[checkpoint_callback])
+                    callbacks=[checkpoint_callback])
+
 
 # %%
 
 tf.train.latest_checkpoint(checkpoint_dir)
 
-model2 = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+model_2 = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
 
-model2.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model_2.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 
-model2.build(tf.TensorShape([1, None]))
+model_2.build(tf.TensorShape([1, None]))
 
-model2.summary()
+model_2.summary()
+
 
 # %%
+# TESTING PHASE
 
 def generate_text(model, start_string):
-  # Evaluation step (generating text using the learned model)
+    # Evaluation step (generating text using the learned model)
 
-  # Number of characters to generate
-  num_generate = 1000
+    # Number of characters to generate
+    num_generate = 1000
 
-  # Converting our start string to numbers (vectorizing)
-  input_eval = [char2idx[s] for s in start_string]
-  input_eval = tf.expand_dims(input_eval, 0)
+    # Converting our start string to numbers (vectorizing)
+    input_eval = [char2idx[s] for s in start_string]
+    input_eval = tf.expand_dims(input_eval, 0)
 
-  # Empty string to store our results
-  text_generated = []
+    # Empty string to store our results
+    text_generated = []
 
-  # Low temperatures results in more predictable text.
-  # Higher temperatures results in more surprising text.
-  # Experiment to find the best setting.
-  temperature = 1.0
+    # Low temperatures results in more predictable text.
+    # Higher temperatures results in more surprising text.
+    # Experiment to find the best setting.
+    temperature = 1.0
 
-  # Here batch size == 1
-  model.reset_states()
-  for i in range(num_generate):
-      predictions = model(input_eval)
-      # remove the batch dimension
-      predictions = tf.squeeze(predictions, 0)
+    # Here batch size == 1
+    model.reset_states()
+    for i in range(num_generate):
+        predictions = model(input_eval)
+        # remove the batch dimension
+        predictions = tf.squeeze(predictions, 0)
 
-      # using a multinomial distribution to predict the word returned by the model
-      predictions = predictions / temperature
-      predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+        # using a multinomial distribution to predict the word returned by the model
+        predictions = predictions / temperature
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
 
-      # We pass the predicted word as the next input to the model
-      # along with the previous hidden state
-      input_eval = tf.expand_dims([predicted_id], 0)
+        # We pass the predicted word as the next input to the model
+        # along with the previous hidden state
+        input_eval = tf.expand_dims([predicted_id], 0)
 
-      text_generated.append(idx2char[predicted_id])
+        text_generated.append(idx2char[predicted_id])
 
-  return (start_string + ''.join(text_generated))
+    return (start_string + ''.join(text_generated))
+
+
+print(generate_text(model_2, start_string=u"ROMEO: "))
 
 # %%
+# TRAINING PHASE
 
-print(generate_text(model2, start_string=u"ROMEO: "))
-
-# %%
-
-model3 = build_model(
-  vocab_size = len(vocab),
-  embedding_dim=embedding_dim,
-  rnn_units=rnn_units,
-  batch_size=BATCH_SIZE)
+model_3 = build_model(
+        vocab_size=len(vocab),
+        embedding_dim=embedding_dim,
+        rnn_units=rnn_units,
+        batch_size=BATCH_SIZE,
+        rnn=RNN)
 
 optimizer = tf.train.AdamOptimizer()
 
@@ -245,28 +245,59 @@ for epoch in range(EPOCHS):
 
     # initializing the hidden state at the start of every epoch
     # initially hidden is None
-    hidden = model3.reset_states()
+    hidden = model_3.reset_states()
 
     for (batch_n, (inp, target)) in enumerate(dataset):
-          with tf.GradientTape() as tape:
-              # feeding the hidden state back into the model
-              # This is the interesting step
-              predictions = model3(inp)
-              loss = tf.losses.sparse_softmax_cross_entropy(target, predictions)
+        with tf.GradientTape() as tape:
+            # feeding the hidden state back into the model
+            # This is the interesting step
+            predictions = model_3(inp)
+            loss = tf.losses.sparse_softmax_cross_entropy(target, predictions)
 
-          grads = tape.gradient(loss, model3.trainable_variables)
-          optimizer.apply_gradients(zip(grads, model3.trainable_variables))
+        grads = tape.gradient(loss, model_3.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model_3.trainable_variables))
 
-          if batch_n % 5 == 0:
-              template = 'Epoch {} Batch {} Loss {:.4f}'
-              print(template.format(epoch+1, batch_n, loss))
+        if batch_n % 5 == 0:
+            template = 'Epoch {} Batch {} Loss {:.4f}'
+            print(template.format(epoch+1, batch_n, loss))
 
     # saving (checkpoint) the model every 1 epoch
     if (epoch + 1) % 1 == 0:
-      model3.save_weights(checkpoint_prefix.format(epoch=epoch))
+        model_3.save_weights(checkpoint_prefix.format(epoch=epoch))
 
-    print ('Epoch {} Loss {:.4f}'.format(epoch+1, loss))
-    print ('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+    print('Epoch {} Loss {:.4f}'.format(epoch+1, loss))
+    print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
 
-model3.save_weights(checkpoint_prefix.format(epoch=epoch))
+model_3.save_weights(checkpoint_prefix.format(epoch=epoch))
 
+
+# %%
+# Examples & Debugging
+
+for i in char_dataset.take(5):
+    print(idx2char[i.numpy()])
+
+for item in sequences.take(5):
+    print(repr(''.join(idx2char[item.numpy()])))
+
+for input_example, target_example in dataset.take(1):
+    print('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+    print('Target data:', repr(''.join(idx2char[target_example.numpy()])))
+
+
+for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
+    print("Step {:4d}".format(i))
+    print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
+    print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
+
+for input_example_batch, target_example_batch in dataset.take(1):
+    example_batch_predictions = model(input_example_batch)
+    print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+
+sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
+sampled_indices = tf.squeeze(sampled_indices, axis=-1).numpy()
+
+example_batch_loss = loss(target_example_batch, example_batch_predictions)
+print("Prediction shape: ", example_batch_predictions.shape,
+      " # (batch_size, sequence_length, vocab_size)")
+print("scalar_loss:      ", example_batch_loss.numpy().mean())
